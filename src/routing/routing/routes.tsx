9c97081ecd4012dc29d2bb7d735dba81096fa000
Kris Gospodinov <kris060601@gmail.com>
2024-02-13 19:29:16 +0200

    Handling errors

--- src/routing/routing/routes.tsx
+++ src/routing/routing/routes.tsx
@@ -3,11 +3,13 @@ import HomePage from "../HomePage";
 import UserListPage from "../UserListPage";
 import UserDetailPage from "../UserDetailPage";
 import Layout from "../Layout";
+import ErrorPage from "../ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { path: "", element: <HomePage /> },
       { path: "users", element: <UserListPage /> },
