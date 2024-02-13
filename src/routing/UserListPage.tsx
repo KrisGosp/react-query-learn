a4480962b8a33c557e1d3e02a828e9d5f0cb92f6
Kris Gospodinov <kris060601@gmail.com>
2024-02-13 19:04:11 +0200

    Exercise

--- src/routing/UserListPage.tsx
+++ src/routing/UserListPage.tsx
@@ -1,19 +1,25 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const UserListPage = () => {
+  const [user, setUser] = useState(1);
   const users = [
     { id: 1, name: "Mosh" },
     { id: 2, name: "John" },
     { id: 3, name: "Alice" },
   ];
   return (
-    <ul className="list-group">
-      {users.map((user) => (
-        <li className="list-group-item" key={user.id}>
-          <Link to={`/users/${user.id}`}>{user.name}</Link>
-        </li>
-      ))}
-    </ul>
+    <div className="flex">
+      <ul className="list-group">
+        {users.map((user) => (
+          <li className="list-group-item" key={user.id}>
+            {/* <Link to={`/users/${user.id}`}>{user.name}</Link> */}
+            <button onClick={() => setUser(user.id)}>{user.name}</button>
+          </li>
+        ))}
+      </ul>
+      <div>User {user}</div>
+    </div>
   );
 };
 
